ef808b14b58edbe72803606628005cd7e68f10d3
san-hub <sk866191@gmail.com>
2021-11-06 12:15:12 +0530

    added TC_00 AND modified TC_customer

---
+++ src/test/java/TC_CS_001.java
@@ -0,0 +1,9 @@
+
+public class TC_CS_001 {
+
+	public static void main(String[] args) {
+		ABE AAISE HI KAR RAHA THA..
+
+	}
+
+}
--- src/test/java/TC_customer_001.java
+++ src/test/java/TC_customer_001.java
@@ -1,2 +1,5 @@
 ///this is a new test case
 this test case is done by some body
+
+
+TO KYUN LIYA THA
